3bd911b65531fbb3a065b3a3dbf5508fc32448a3
Nik <Nik@DESKTOP-3UJDSVQ>
2024-02-25 20:35:43 +0300

    All ready

--- TestingCons/Program.cs
+++ TestingCons/Program.cs
@@ -2,6 +2,5 @@
 
 Calculation calculation = new Calculation();
 Console.WriteLine(calculation.Square(1));
-Console.WriteLine(calculation.Square(1,2));
 Console.WriteLine(calculation.Square(2,3,4));
 Console.ReadLine();
